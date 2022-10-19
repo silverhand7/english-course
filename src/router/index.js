7959a5c66a175aabfd493461f0c7526cd4b6df09
silverhand <refojunior5@gmail.com>
2022-10-19 22:51:02 +0800

    clean up route index

--- src/router/index.js
+++ src/router/index.js
@@ -6,22 +6,9 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/course',
-      name: 'Meta',
+      name: 'Home',
       component: () => import('../views/CourseView.vue')
-    }
+    },
   ]
 })
 
